1c26c0b3907fca9e824b372fbe0675be94aded23
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-03 01:28:16 +0530

    Time: 10 ms (8.85%), Space: 44 MB (7.36%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> temp = new ArrayList<>();
+        printSub(0, temp, nums, ans);
+        return ans;
+    }
+    void printSub(int index, List<Integer> temp, int[] nums, List<List<Integer>> ans){
+        // Base case
+        if(index >= nums.length){
+            ans.add(new ArrayList<>(temp));
+            return;
+        }
+        
+        // Pick element at index
+        temp.add(nums[index]);
+        printSub(index+1, temp, nums, ans);
+        
+        // Not Pick element at index
+        System.out.println(index);
+        temp.remove(temp.size()-1); // Backtrack
+        printSub(index+1, temp, nums, ans);
+    }
+}
