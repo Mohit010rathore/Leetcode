406cd6d2105d7cfbb87141d0adb77cfe4cb129f9
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-03 13:03:51 +0530

    Time: 21 ms (47.74%), Space: 61.1 MB (39.40%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.java
@@ -3,15 +3,17 @@ class Solution {
        Arrays.sort(horizontalCuts);
         Arrays.sort(verticalCuts);
 
-
-        int maxHeight = Math.max(horizontalCuts[0], h - horizontalCuts[horizontalCuts.length-1]);
+        int maxHeight = horizontalCuts[0];
+        
+        maxHeight = Math.max(maxHeight, h - horizontalCuts[horizontalCuts.length-1]);
         for (int i = 1;i<horizontalCuts.length; i++){
             int horizatalWidth = horizontalCuts[i] - horizontalCuts[i-1];
             maxHeight = Math.max(maxHeight, horizatalWidth);
         }
-
-
-        int maxWidth = Math.max(verticalCuts[0], w - verticalCuts[verticalCuts.length-1]);
+        
+        int maxWidth = verticalCuts[0];
+        
+        maxWidth = Math.max(maxWidth, w - verticalCuts[verticalCuts.length-1]);
         for (int i = 1;i<verticalCuts.length; i++){
             int verticalWidth = verticalCuts[i] - verticalCuts[i-1];
             maxWidth = Math.max(maxWidth, verticalWidth);
