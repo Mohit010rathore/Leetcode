0c90eb2be46cd6982cbce405a8146693dae6b439
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-11 19:46:59 +0530

    Time: 217 ms (12.79%), Space: 132.7 MB (9.54%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,16 +1,18 @@
 class Solution {
-    public int f(int ind,int buy,int cap,int [] prices,int n,int [][][] dp){
-        if(cap ==0 || ind == n){
-            return 0;
-        }
-        if(dp[ind][buy][cap] > 0){
-            return dp[ind][buy][cap];
-        }
-        if(buy == 1){
-            return dp[ind][buy][cap] = Math.max(-prices[ind] + f(ind+1,0,cap,prices,n,dp), 0 + f(ind+1,1,cap,prices,n,dp));
-        }
-        return dp[ind][buy][cap] = Math.max(prices[ind] + f(ind+1,1,cap-1,prices,n,dp), 0 + f(ind+1,0,cap,prices,n,dp));
-    }
+    //memoized
+    // public int f(int ind,int buy,int cap,int [] prices,int n,int [][][] dp){
+    //     if(cap ==0 || ind == n){
+    //         return 0;
+    //     }
+    //     if(dp[ind][buy][cap] > 0){
+    //         return dp[ind][buy][cap];
+    //     }
+    //     if(buy == 1){
+    //         return dp[ind][buy][cap] = Math.max(-prices[ind] + f(ind+1,0,cap,prices,n,dp), 0 + f(ind+1,1,cap,prices,n,dp));
+    //     }
+    //     return dp[ind][buy][cap] = Math.max(prices[ind] + f(ind+1,1,cap-1,prices,n,dp), 0 + f(ind+1,0,cap,prices,n,dp));
+    // }
+    //tabulated - no need to write bases if want u can- bcoz base cases are already assigned as 0 whe we declared dp. 
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int dp[][][] = new int [n+1][2][3];
