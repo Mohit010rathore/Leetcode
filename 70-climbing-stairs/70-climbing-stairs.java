7798b7474df2bbc92c8289f8309150f214078ef6
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-12 13:14:17 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (71.00%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,13 +1,23 @@
 class Solution {
     int [] dp = new int[46];
+    // public int climbStairs(int n) {
+    //     if(n <= 1){
+    //         return 1;
+    //     }
+    //     if(dp[n] != 0){
+    //         return dp[n];
+    //     }else{
+    //         return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+    //     }
+    // }
     public int climbStairs(int n) {
         if(n <= 1){
             return 1;
         }
-        if(dp[n] != 0){
+        if(dp[n] > 0){
             return dp[n];
-        }else{
-            return dp[n] = climbStairs(n-1) + climbStairs(n-2);
         }
-    }
+        
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+    }   
 }
