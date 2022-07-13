ebcb033b8b5c6126ed779f5061e247c4ed4865a8
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-13 22:47:33 +0530

    Time: 1 ms (38.39%), Space: 41.3 MB (69.03%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -17,19 +17,36 @@ class Solution {
         return prev;
         
     }
-    public int rob(int[] nums) {
+//     public int rob(int[] nums) {
+//         int n = nums.length;
+//         int[] temp1= new int[n];
+//         int[] temp2 = new int[n];
+//         if(n==1){
+//            return nums[0];
+//         }
+        
+//         for(int i=0;i<n;i++){
+//             if(i != 0) temp1[i] += nums[i];
+//             if(i != n-1) temp2[i] += nums[i];
+//         }
+//         return Math.max(helper(temp1),helper(temp2));
+        
+//     }
+    
+    public int rob(int[] nums){
         int n = nums.length;
-        int[] temp1= new int[n];
-        int[] temp2 = new int[n];
-        if(n==1){
-           return nums[0];
+        int[] temp1 = new int[n];
+        int [] temp2 = new int [n];
+        if(n == 1){
+            return nums[0];
         }
         
-        for(int i=0;i<n;i++){
-            if(i != 0) temp1[i] += nums[i];
+        for(int i = 0;i< n;i++){
+            if(i != 0) temp1[i]+= nums[i];
             if(i != n-1) temp2[i] += nums[i];
         }
-        return Math.max(helper(temp1),helper(temp2));
         
+        return Math.max(helper(temp1), helper(temp2));
     }
+    
 } 
