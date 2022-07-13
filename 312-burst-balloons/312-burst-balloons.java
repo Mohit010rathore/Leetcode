25789a95d733301e1a38fa1d1827a32a71e74954
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-14 01:25:47 +0530

    Time: 124 ms (56.52%), Space: 41.6 MB (96.95%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int f(int i ,int j,int a[],int dp[][]){
+        if(i>j) return 0;
+        int maxi = Integer.MIN_VALUE;
+        
+        if(dp[i][j] > 0){
+            return dp[i][j] ;
+        }
+        for(int ind =i;ind <= j;ind++){
+            int cost = a[i-1] * a[ind] * a[j+1] + f(i,ind-1,a,dp) + f(ind+1,j,a,dp);
+            maxi= Math.max(cost,maxi);
+            
+        }
+        return dp[i][j] =  maxi;
+    }
+    public int maxCoins(int[] nums) {
+        int n = nums.length;
+        int[] nCuts = new int[n + 2];
+        nCuts[0] = 1;
+        nCuts[n + 1] = 1;
+        System.arraycopy(nums, 0, nCuts, 1, n);
+        
+        int dp[][] = new int[n+1][n+1];
+        return f(1,n,nCuts,dp);
+    }
+}
