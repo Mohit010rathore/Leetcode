eb0a4b22ffbebf821d5a75238cc31e6e28b5f630
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-13 21:59:12 +0530

    Time: 10 ms (99.02%), Space: 41.8 MB (93.62%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.java
@@ -1,19 +1,19 @@
 class Solution {
-    public int f(int i,int j,int[] nCuts,int[][] dp){
-        if(i>j){
-            return 0;
-        }
-        if(dp[i][j] > 0){
-            return dp[i][j];
-        }
-        int mini = Integer.MAX_VALUE;
-        for(int ind = i;ind<=j;ind++){
-            int cost = nCuts[j+1]-nCuts[i-1] + f(i,ind-1,nCuts,dp) + f(ind+1,j,nCuts,dp);
+//     public int f(int i,int j,int[] nCuts,int[][] dp){
+//         if(i>j){
+//             return 0;
+//         }
+//         if(dp[i][j] > 0){
+//             return dp[i][j];
+//         }
+//         int mini = Integer.MAX_VALUE;
+//         for(int ind = i;ind<=j;ind++){
+//             int cost = nCuts[j+1]-nCuts[i-1] + f(i,ind-1,nCuts,dp) + f(ind+1,j,nCuts,dp);
             
-            mini= Math.min(mini,cost);
-        }
-        return dp[i][j]  =  mini;
-    }
+//             mini= Math.min(mini,cost);
+//         }
+//         return dp[i][j]  =  mini;
+    // }
     public int minCost(int n, int[] cuts) {
         int c = cuts.length;
         int[] nCuts = new int[c + 2];
@@ -22,8 +22,24 @@ class Solution {
         System.arraycopy(cuts, 0, nCuts, 1, c);
         Arrays.sort(nCuts);
         
-        int dp[][] = new int[c+1][c+1];
+        // int dp[][] = new int[c+1][c+1];
         
-        return f(1,c,nCuts,dp);
+        int dp[][] = new int[c+2][c+2];
+        
+        // return f(1,c,nCuts,dp);
+        for(int i =c;i>= 1;i--){
+            for(int j = 1;j<= c;j++){
+                if(i > j) continue;
+                
+                int mini = Integer.MAX_VALUE;
+                for(int ind = i;ind<=j;ind++){
+                    int cost = nCuts[j+1]-nCuts[i-1] + dp[i][ind-1] + dp[ind+1][j];
+            
+                    mini= Math.min(mini,cost);
+                }
+                dp[i][j]  =  mini;
+            }
+        }
+        return dp[1][c];
     }
 }
