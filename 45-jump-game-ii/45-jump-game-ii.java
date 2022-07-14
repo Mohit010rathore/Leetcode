502bdcfdf9efcd3fc90329b784a2408526c1c43d
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-15 00:42:42 +0530

    Time: 187 ms (14.49%), Space: 50 MB (20.77%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,19 +1,44 @@
-class Solution {
-    public int jump(int[] nums) {
-        int n =  nums.length;
-        int pos= 0;
-        int des = 0;
-        int count = 0;
+// class Solution {
+//     public int f(int start ,int [] nums){
+//         if(start >= nums.length){
+//             return 0;
+//         }
         
-        for(int i=0;i<n-1;i++){
-            des = Math.max(des,i+nums[i]);
+//         int minJump = Integer.MAX_VALUE;
+//         for(int i = start + 1;i<start+nums[i];i++)
+//     }
+//     public int jump(int[] nums) {
+//         int n =  nums.length;
+//         int pos= 0;
+//         int des = 0;
+//         int count = 0;
+        
+//         for(int i=0;i<n-1;i++){
+//             des = Math.max(des,i+nums[i]);
             
-            if(pos == i){
-                pos = des;
-                count++;
-            }
-        }
-        return count;
+//             if(pos == i){
+//                 pos = des;
+//                 count++;
+//             }
+//         }
+//         return count;
         
+//     }
+// }
+class Solution {
+    Integer dp[];
+    public int helper(int i,int[] nums){
+        if(i==nums.length-1) return 0;
+        if(i>=nums.length) return Integer.MAX_VALUE-1;
+        if(dp[i]!=null) return dp[i];
+        int minJumps=Integer.MAX_VALUE-1;
+        for(int k=1;k<=nums[i];k++){
+            minJumps=Math.min(1+helper(i+k,nums),minJumps);
+        }
+        return dp[i]=minJumps;
+    }
+    public int jump(int[] nums) {
+        dp=new Integer[nums.length];
+        return helper(0,nums);
     }
 }
