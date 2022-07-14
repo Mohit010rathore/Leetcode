b9d141d20e06ef66fcff2ae706af986183cb5331
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-14 22:37:31 +0530

    Time: 1820 ms (14.92%), Space: 42 MB (80.79%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.java
@@ -15,8 +15,6 @@ class Solution {
         }
         int minCost = Integer.MAX_VALUE;
         
-        String temp = "";
-        
         //i.....j
         for(int j =i;j<n;j++){
             if(isPalindrome(i,j,s)){
@@ -29,7 +27,23 @@ class Solution {
     }
     public int minCut(String s) {
         int n = s.length();
-        int dp[] = new int [n];
-        return f(0,n,s,dp) - 1;
+        //int dp[] = new int [n];
+        //return f(0,n,s,dp) - 1;
+        
+        int dp[] = new int [n+1];
+        dp[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            int minCost = Integer.MAX_VALUE;
+        
+            for(int j =i;j<n;j++){
+                 if(isPalindrome(i,j,s)){
+                     int cost = 1 + dp[j+1];
+                
+                     minCost = Math.min(cost,minCost);
+                 }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0]-1;
     }
 }
