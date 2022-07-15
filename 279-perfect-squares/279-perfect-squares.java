45b91331fd7d8e911b6cb8a468cfa6f208b63422
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-15 23:50:01 +0530

    Time: 134 ms (29.70%), Space: 43.9 MB (37.31%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.java
+++ 279-perfect-squares/279-perfect-squares.java
@@ -13,18 +13,8 @@ class Solution {
         }
         return dp[n] = ans;
     }
-public int numSquares(int n) {
-    int []dp=new int[n+1];
-    dp[0]=0;
-    for(int i=1;i<=n;i++)
-    {
-        dp[i]=i;
-        for(int j=1;j*j<=i;j++)
-        {
-            int val=j*j;
-            dp[i]=Math.min(dp[i],1+dp[i-val]);
-        }
+    public int numSquares(int n) {
+        int dp[] = new int[n+1];
+        return f(n,dp);
     }
-    return dp[n];
-}
 }
