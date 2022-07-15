4ce9bb8a5bcc5afac47fec2af36aab7aaa71ed90
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-15 23:01:31 +0530

    Time: 3 ms (56.12%), Space: 47.8 MB (71.30%) - LeetHub

--- 338-counting-bits/338-counting-bits.java
+++ 338-counting-bits/338-counting-bits.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[] countBits(int n) {
         
+        //ans[i] array at i -> stores no. of 1's from 0 to i
         int [] ans = new int[n+1];
         ans[0] = 0;
         
         for(int i=1;i<=n;i++){
-            ans[i] = ans[i/2] + i%2;
+            ans[i] = ans[i/2] + i%2; // i%2 adds 1 to the numbers which are odd and adds 0 if they are even
         }
         return ans;
     }
