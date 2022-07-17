b9a8ce17cd339cd301aebfd9a71e2bfb1b70af43
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-18 01:08:59 +0530

    Time: 25 ms (20.08%), Space: 54.5 MB (11.07%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,29 +1,52 @@
+//class Solution {
+    // public String reverseWords(String s) {
+//         int  i=s.length()-1;
+        
+//         StringBuilder res= new StringBuilder("");
+        
+//         while(i >= 0){
+//             while(i >= 0 && s.charAt(i) == ' '){
+//                 i--;
+//             }
+//             int j = i;
+            
+//             if(i < 0){
+//                 break;
+//             } 
+//             while(i >= 0 && s.charAt(i) != ' '){
+//                 i--;
+//             }
+            
+//             if(res.isEmpty()){
+//                 res = res.append(s.substring(i+1,j+1));
+//             }else{
+//                 res = res.append(" " + s.substring(i+1,j+1));
+//             }
+//         }
+//         return res.toString();
+        
+//     }
 class Solution {
     public String reverseWords(String s) {
-        int  i=s.length()-1;
-        
-        StringBuilder res= new StringBuilder("");
-        
-        while(i >= 0){
-            while(i >= 0 && s.charAt(i) == ' '){
-                i--;
-            }
-            int j = i;
-            
-            if(i < 0){
+        String result = new String();
+        int start=0;
+        int n=s.length();
+        while(start<n){
+            while(start<n && s.charAt(start)== ' ')
+                start++;
+            if(start>=n)
                 break;
-            } 
-            while(i >= 0 && s.charAt(i) != ' '){
-                i--;
-            }
-            
-            if(res.isEmpty()){
-                res = res.append(s.substring(i+1,j+1));
-            }else{
-                res = res.append(" " + s.substring(i+1,j+1));
-            }
+            int end = start+1;
+            while(end<n && s.charAt(end)!= ' ')
+                end++;
+            String sub = s.substring(start,end);
+            if(result.length()==0)
+                result = sub;
+            else
+                result = sub + " " + result;
+            start = end+1;
         }
-        return res.toString();
-        
+        return result;
     }
+
 }
