db49bdbcc0901466d7eaeb8301d8c16dc0960926
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-18 01:27:14 +0530

    Time: 10 ms (58.62%), Space: 43.8 MB (53.97%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,52 +1,56 @@
-//class Solution {
-    // public String reverseWords(String s) {
-//         int  i=s.length()-1;
-        
-//         StringBuilder res= new StringBuilder("");
-        
-//         while(i >= 0){
-//             while(i >= 0 && s.charAt(i) == ' '){
-//                 i--;
-//             }
-//             int j = i;
-            
-//             if(i < 0){
-//                 break;
-//             } 
-//             while(i >= 0 && s.charAt(i) != ' '){
-//                 i--;
-//             }
-            
-//             if(res.isEmpty()){
-//                 res = res.append(s.substring(i+1,j+1));
-//             }else{
-//                 res = res.append(" " + s.substring(i+1,j+1));
-//             }
-//         }
-//         return res.toString();
-        
-//     }
 class Solution {
     public String reverseWords(String s) {
-        String result = new String();
-        int start=0;
-        int n=s.length();
-        while(start<n){
-            while(start<n && s.charAt(start)== ' ')
-                start++;
-            if(start>=n)
+        int  start=s.length()-1; //we started from the last
+        
+        StringBuilder res= new StringBuilder("");
+        
+        while(start >= 0){
+            while(start >= 0 && s.charAt(start) == ' '){
+                start--;
+            }
+            int end = start; //here when wee found the character of a word starting from right we assign it to end-it will represent the word ending
+            
+            if(start < 0){  //when we arrived at first index and still going on after it...this condition will block it to move
                 break;
-            int end = start+1;
-            while(end<n && s.charAt(end)!= ' ')
-                end++;
-            String sub = s.substring(start,end);
-            if(result.length()==0)
-                result = sub;
-            else
-                result = sub + " " + result;
-            start = end+1;
+            } 
+            while(start >= 0 && s.charAt(start) != ' '){ //and we again move i to find the starting of the word, and store it in start
+                start--;
+            }
+            
+            if(res.isEmpty()){
+                res = res.append(s.substring(start+1,end+1));  //starting from -1 bcoz i is on space and j+1 to include the whole word.
+            }else{
+                res = res.append(" " + s.substring(start+1,end+1));  //concat with space
+            }
         }
-        return result;
+        return res.toString();
+        
     }
-
 }
+// class Solution {
+//     public String reverseWords(String s) {
+//         int start = s.length()-1;
+        
+//         String ans ="";
+//         while(start >= 0){
+//             while(start >= 0 && s.charAt(start) == ' '){
+//                 start--;
+//             }
+//             int end = start;
+            
+//             while(start >= 0 && s.charAt(start) == ' '){
+//                 start--;
+//             }
+            
+//             if(start < 0) break; //when we arrived at first index and still going on after it...this condition will block it to move
+            
+//             if(ans.isEmpty()){
+//                 ans = ans.concat(s.substring(start+1,end+1)); //starting from -1 bcoz i is on space and j+1 to include the whole word.
+//             }else{
+//                 ans = ans.concat(" " + s.substring(start+1,end+1)) ;//concat with space
+//             }
+//         }
+//         return ans;
+//     }
+
+// }
