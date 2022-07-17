5bd61d5c0e93b3ab881a61966fd9f8bc646cbc42
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-18 00:15:32 +0530

    Time: 14 ms (71.84%), Space: 48.6 MB (66.66%) - LeetHub

---
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    static final int CHAR = 256;
+    public int firstUniqChar(String s) {
+        int fI[] = new int [CHAR];
+        Arrays.fill(fI,-1);
+        
+        for(int i=0;i<s.length();i++){
+            if(fI[s.charAt(i)] == -1)
+            {
+                fI[s.charAt(i)] = i;
+            }else
+            {
+                fI[s.charAt(i)] = -2;
+            }
+        }
+        int res = Integer.MAX_VALUE;
+        for(int i=0;i<CHAR;i++){
+            if(fI[i] >= 0){
+                res = Math.min(res,fI[i]);
+            }
+        }
+        return (res == Integer.MAX_VALUE) ? -1 : res;
+    }
+}
