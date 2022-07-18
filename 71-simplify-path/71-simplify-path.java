6e67aae3c742e5218fa2645f486dbdbfacf57d12
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-19 01:45:09 +0530

    Time: 15 ms (22.45%), Space: 45.5 MB (9.47%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack st = new Stack<>();
+        String[] pathList = path.split("\\/");  //this is the way of writing since we need to escape the backslash
+        
+        for(String p: pathList){
+            if(p.length() == 0 || p.equals(".")){  //do nothing
+                continue;
+            }else if(p.equals("..")){  //move to upper directory - popping from the stack
+                if(!st.isEmpty()){
+                    st.pop();
+                }
+            }else{   //if its a string character we push into the stack
+                //String case
+                st.push(p);
+            }
+        }
+        String ans = "";
+        while(!st.isEmpty()){
+            ans = st.pop() + "/" + ans;  //whatever we are popping adding it in the front of the ans with one backslash
+        }
+        
+        if(ans.length() == 0){  //corner case id the ans string comes out to be empty we return backslash
+            return "/";
+        }else{
+            return "/" + ans.substring(0,ans.length()-1); //we just removing the last backslash and letting it in front
+        }
+    }
+}
