9b9c8a94ac6e34a3c6533594528be1f81723d968
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-19 12:02:21 +0530

    Time: 1 ms (100.00%), Space: 43.2 MB (20.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int[] numberOfPairs(int[] nums) {
         int n = nums.length;
-        int[] freq = new int[101];
+        int[] freq = new int[101]; //creatrred a freq array to store frq of nums array elements
         int [] ans = new int[2];
         
         for(int i =0;i<n;i++){
@@ -9,8 +9,8 @@ class Solution {
         }
         
         for(int i=0;i<freq.length;i++){
-            ans[1] += freq[i] % 2;
-            ans[0] += freq[i] / 2;
+            ans[1] += freq[i] % 2;  //if the there is remainder,means there is some elemnts which cant be paired coz they are odd in numbers
+            ans[0] += freq[i] / 2;  //if the freq is divisible by 2 and given remainder 0,means there are even number of duplicates
         }
         
         return ans;
