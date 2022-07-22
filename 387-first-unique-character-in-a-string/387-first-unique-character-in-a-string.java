26a26cb58920a47cd12fb2b3f863b1ab3537569b
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-22 15:25:23 +0530

    Time: 13 ms (73.72%), Space: 48.4 MB (70.04%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -1,24 +1,58 @@
-class Solution {
-    static final int CHAR = 256;
-    public int firstUniqChar(String s) {
-        int fI[] = new int [CHAR];
-        Arrays.fill(fI,-1);
+// class Solution {
+//     static final int CHAR = 256;
+//     public int firstUniqChar(String s) {
+//         int fI[] = new int [CHAR];
+//         Arrays.fill(fI,-1);
         
-        for(int i=0;i<s.length();i++){
-            if(fI[s.charAt(i)] == -1)
-            {
-                fI[s.charAt(i)] = i;
-            }else
-            {
-                fI[s.charAt(i)] = -2;
+//         for(int i=0;i<s.length();i++){
+//             if(fI[s.charAt(i)] == -1)
+//             {
+//                 fI[s.charAt(i)] = i;
+//             }else
+//             {
+//                 fI[s.charAt(i)] = -2;
+//             }
+//         }
+//         int res = Integer.MAX_VALUE;
+//         for(int i=0;i<CHAR;i++){
+//             if(fI[i] >= 0){
+//                 res = Math.min(res,fI[i]);
+//             }
+//         }
+//         return (res == Integer.MAX_VALUE) ? -1 : res;
+//     }
+// }
+class Solution {
+    public int firstUniqChar(String s) {
+        int [] freq = new int[26];
+        
+        for(int i =0;i <s.length();i++){
+            freq[s.charAt(i) - 'a'] ++;
+            
+        }
+        
+        for(int i =0;i<s.length() ;i++){
+            if(freq[s.charAt(i) - 'a'] == 1){
+                return i;
             }
         }
-        int res = Integer.MAX_VALUE;
-        for(int i=0;i<CHAR;i++){
-            if(fI[i] >= 0){
-                res = Math.min(res,fI[i]);
-            }
-        }
-        return (res == Integer.MAX_VALUE) ? -1 : res;
+        return -1;
+        
     }
 }
+
+
+// class Solution {
+//     private static final int SIZE=26;
+//     public int firstUniqChar(String s) {
+//        int freq[]=new int[SIZE];
+//         for(int i=0;i<s.length();i++){
+//             freq[s.charAt(i)-'a']++;
+//         }
+//         for(int i=0;i<s.length();i++){
+//             if(freq[s.charAt(i)-'a']==1)
+//                 return i;
+//         }
+//         return -1;
+//     }
+// }
