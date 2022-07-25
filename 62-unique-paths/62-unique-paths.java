4e23079da95fec71346af13b08386ab5657558b5
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-26 02:25:05 +0530

    Time: 1 ms (53.06%), Space: 41.2 MB (26.87%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,20 +1,22 @@
 class Solution {
-    public int f(int i,int j,int[][] dp){
-        if(i==0 && j==0){
-            return 1;
-        }
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(dp[i][j] > 0){
-            return dp[i][j];
-        }
-        int up = f(i-1,j,dp);
-        int left = f(i,j-1,dp);
+//     public int f(int i,int j,int[][] dp){
+//         if(i==0 && j==0){
+//             return 1;
+//         }
+//         if(i<0 || j<0){
+//             return 0;
+//         }
+//         if(dp[i][j] > 0){
+//             return dp[i][j];
+//         }
+//         int up = f(i-1,j,dp);
+//         int left = f(i,j-1,dp);
         
-        return dp[i][j] = up+left;
-    }
+//         return dp[i][j] = up+left;
+//     }
     public int uniquePaths(int m, int n) {
+        // int dp[][] = new int[m][n]; 
+        // return f(m-1,n-1,dp);
         int dp[][] = new int[m][n];
         for(int i =0;i<m;i++){
             for(int j = 0;j<n;j++){
@@ -33,6 +35,6 @@ class Solution {
                 }
             }
         }
-        return dp[m-1][n-1];
+       return dp[m-1][n-1];
     }
 }
