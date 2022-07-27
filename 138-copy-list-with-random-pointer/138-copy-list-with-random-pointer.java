7b3be40cf56e0a88f6be1bc6d3fcc7fe68887863
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-27 20:38:00 +0530

    Time: 0 ms (100.00%), Space: 45 MB (79.31%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -15,42 +15,81 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
-        Node curr = head, temp = head;
+//         Node curr = head, temp = head;
         
-        // Step 1: Insert copy nodes right after original nodes
-        while(curr != null){
-            temp = curr.next; 
-            // Node copy = new Node(curr.val);
-            // curr.next = copy;
-            // copy.next = temp;
-            // curr = temp;
+//         // Step 1: Insert copy nodes right after original nodes
+//         while(curr != null){
+//             temp = curr.next; 
+//             curr.next = new Node(curr.val);
+//             curr.next.next = temp;
+//             curr = temp;
+//         }
+        
+//         // Step 2: Point the random pointers
+//         curr = head;
+//         while(curr != null){
+//             if(curr.random != null)
+//                 curr.next.random = curr.random.next;
+//             curr = curr.next.next;
+//         }
+        
+//         // Step 3: Get the original and deep copy linked list
+//         curr = head;
+//         Node copyHead = new Node(0);
+//         Node copy = copyHead;
+        
+//         while(curr != null){
+//             temp = curr.next.next;
+//             copy.next = curr.next;
+//             curr.next = temp;
+//             copy = copy.next;
+//             curr = curr.next;
+//         }
+        
+//         return copyHead.next;
+        if(head==null) return head;
+        Node curr = head, temp = null;
+ 
+        // insert additional node after
+        // every node of original list
+        while (curr != null) {
+            temp = curr.next;
+ 
+            // Inserting node
             curr.next = new Node(curr.val);
             curr.next.next = temp;
             curr = temp;
         }
-        
-        // Step 2: Point the random pointers
         curr = head;
-        while(curr != null){
-            if(curr.random != null)
-                curr.next.random = curr.random.next;
-            curr = curr.next.next;
+ 
+        // adjust the random pointers of the
+        // newly added nodes
+        while (curr != null) {
+            if (curr.next != null)
+                curr.next.random = (curr.random != null)
+                                       ? curr.random.next
+                                       : curr.random;
+ 
+            // move to the next newly added node by
+            // skipping an original node
+            curr = curr.next.next;                   
         }
-        
-        // Step 3: Get the original and deep copy linked list
-        curr = head;
-        Node copyHead = new Node(0);
-        Node copy = copyHead;
-        
-        while(curr != null){
-            temp = curr.next.next;
-            copy.next = curr.next;
-            curr.next = temp;
+ 
+        Node original = head, copy = head.next;
+ 
+        // save the start of copied linked list
+        temp = copy;
+ 
+        // now separate the original list and copied list
+        while (original != null) {
+            original.next =original.next.next;
+ 
+          copy.next = (copy.next != null) ? copy.next.next
+                                            : copy.next;
+            original = original.next;
             copy = copy.next;
-            curr = curr.next;
         }
-        
-        return copyHead.next;
+        return temp;
         
     }
 }
