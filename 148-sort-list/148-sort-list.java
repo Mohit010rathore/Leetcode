42c4f0f8aa665e35ec2664adb326fccbb405bcc0
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-07-28 21:26:01 +0530

    Time: 19 ms (49.02%), Space: 73.1 MB (82.35%) - LeetHub

---
+++ 148-sort-list/148-sort-list.java
@@ -0,0 +1,76 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode merge(ListNode left,ListNode right){
+        if(left == null) return right;
+        if(right == null) return left;
+        
+        ListNode ans = new ListNode(0);
+        ListNode temp = ans;
+        
+        while(left != null && right != null){
+            if(left.val < right.val){
+                temp.next = left;
+                temp = left;
+                left = left.next;
+            }
+            else{
+                temp.next = right;
+                temp = right;
+                right = right.next;
+            }
+        }
+        
+        while(left != null){
+            temp.next = left;
+            temp = left;
+            left= left.next;
+        }
+        while(right != null){
+            temp.next = right;
+            temp = right;
+            right= right.next;
+        }
+        ans = ans.next;
+        return ans;
+        
+    }
+
+    public ListNode sortList(ListNode head) {
+        
+        if(head == null || head.next == null){
+            return head;
+        }
+        
+        ListNode mid = findMid(head);
+        
+        ListNode left = head;
+        ListNode right = mid.next;
+        mid.next = null;
+        
+        left = sortList(left);
+        right = sortList(right);
+        
+        ListNode result = merge(left,right);
+        
+        return result;
+    }
+    public ListNode findMid(ListNode head){
+       ListNode slow = head;
+        ListNode fast = head.next;
+        
+        while(fast != null && fast.next != null){
+            slow= slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
