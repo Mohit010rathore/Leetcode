1665a9b1e9a2a2376c80b99d9b9231f8cc789889
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-11 14:23:09 +0530

    Time: 165 ms (23.90%), Space: 75.6 MB (5.69%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.java
@@ -1,6 +1,13 @@
 class Solution{
     private final long MOD = 1_000_000_007;
     
+    //what we do is for every particular element we find its prpevious smaller element(index) and next smaller element(index)
+    
+    //bcoz for that element ,that element, will be smaller for the particular subarray(from prev smaller and next smaller) till we found smaller element on right and smaller element on left
+    
+    //to calculate number of elements on left and right, for left it will be (i) and  for right it will be ( arr.length - i -1 )
+    
+    //and to calculate ans =>( arr[i] * lengthRight + 1 * lengthLeft + 1 )
     public int sumSubarrayMins(int[] arr) {
         Stack<Integer> st = new Stack<Integer>();
         int[] leftDist = new int[arr.length];
