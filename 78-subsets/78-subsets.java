3ff412af641552e6771891c0a0b2a0f50b7f0855
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-19 14:38:27 +0530

    Time: 2 ms (39.83%), Space: 43 MB (79.34%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,24 +1,17 @@
 class Solution {
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> temp = new ArrayList<>();
-        printSub(0, temp, nums, ans);
-        return ans;
-    }
-    void printSub(int index, List<Integer> temp, int[] nums, List<List<Integer>> ans){
-        // Base case
-        if(index >= nums.length){
-            ans.add(new ArrayList<>(temp));
-            return;
+    public void func(int ind,int []nums,ArrayList<Integer> ds, List<List<Integer>> subList){
+        subList.add(new ArrayList<>(ds));
+        for(int i = ind;i< nums.length;i++){
+            
+            ds.add(nums[i]);
+            func(i+1,nums,ds,subList);
+            ds.remove(ds.size()-1);
         }
-        
-        // Pick element at index
-        temp.add(nums[index]);
-        printSub(index+1, temp, nums, ans);
-        
-        // Not Pick element at index
-        // System.out.println(index);
-        temp.remove(temp.size()-1); // Backtrack
-        printSub(index+1, temp, nums, ans);
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> subList = new ArrayList<>();
+        func(0,nums,new ArrayList<>(),subList);
+        return subList;
     }
 }
