ffdac5d3f3b4d7cdc84cc8b1504485448b6121df
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-21 00:25:19 +0530

    Time: 4 ms (43.24%), Space: 41.5 MB (70.55%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.java
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -3,23 +3,44 @@ class Solution {
         int fact = 1;
         List<Integer> numbers = new ArrayList<>();
         
+        //factorial 
         for(int i =1;i<n;i++){
             fact = fact * i;
+            //storing all the numbers {1,2,3}
             numbers.add(i);
         }
+        //storing the last number here {1,2,3,4}
         numbers.add(n);
         
+        //we have to return ans in a string 
         String ans = "";
+        
+        //as we are considering 0-based indexing
+        //k = 17
+        //k = 17-1 = 16
         k = k-1;
+        
+        //running an infinite loop
         while(true){
+            
+            //16(k) / 6(3!) = 2nd index number = 3-> we pick out and add to our ans
             ans = ans + numbers.get(k/fact);
+            
+            //remove this 3 to get the new array {1,2,4}
             numbers.remove(k/fact);
+            
+            //till the list is empty
             if(numbers.size() == 0){
                 break;
             }
             
+            //16(k) % 6{fact 3!}
             k = k % fact ;
+            
+             // 6 / 3 = 2 (new numbers array size)
             fact = fact / numbers.size();
+            
+            //doing these tasks every time till the numbers list becomees empty we break and return ans
         }
         return ans;
     }
