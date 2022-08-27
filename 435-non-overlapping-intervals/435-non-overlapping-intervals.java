b587b5e21e5481e43a7529435a7b00ac436c4a7f
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-27 13:43:00 +0530

    Time: 81 ms (70.58%), Space: 100.3 MB (61.87%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.java
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.java
@@ -1,20 +1,26 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
+        //sorting all the intervals in the ascending orders
          Arrays.sort(intervals, (arr1, arr2) -> Integer.compare(arr1[0], arr2[0]));
         
-        int count= 0;
-        int n = intervals.length;
-        int left = 0;
-        int right = 1;
         
-        while(right < n){
-            if(intervals[left][1] <= intervals[right][0]){
+        int count= 0; // count of no. of intervals to be removed
+        int n = intervals.length; // number of intervals
+        int left = 0; //left interval
+        int right = 1; // right interval
+        
+        while(right < n){  //unless all intervals are compared
+            if(intervals[left][1] <= intervals[right][0]){ //Non- overlapping case
                 left = right;
                 right += 1;
-            }else if(intervals[left][1] <= intervals[right][1]){
-                count+= 1;
+            }
+            //Overlapping case 1(REmove right interval)
+            else if(intervals[left][1] <= intervals[right][1]){
+                count+= 1; // delete right
                 right += 1;
-            }else if(intervals[left][1] > intervals[right][1]){
+            }
+            //Overlapping case2- (Remove left interval)
+            else if(intervals[left][1] > intervals[right][1]){
                 count += 1;
                 left = right;
                 right += 1;
