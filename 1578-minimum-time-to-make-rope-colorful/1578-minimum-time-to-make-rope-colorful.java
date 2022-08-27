20832511f54476f9723f67a380eb97d716490c93
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-28 00:22:20 +0530

    Time: 22 ms (15.51%), Space: 89.5 MB (66.11%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,18 +1,24 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        char c = '*';
+        char c = '*'; //initially taken '*'
         int lastTime = 0;
         int ans = 0;
         
         for(int i = 0;i<colors.length();i++){
-            if(colors.charAt(i) == c){
-                if(lastTime < neededTime[i]){
+            
+            if(colors.charAt(i) == c){    //if the prev character is same
+                
+                if(lastTime < neededTime[i]){    //and the Time required in prev is less than current time, we just add the lasttime in our ans and update our lastTime
                     ans += lastTime;
                     lastTime = neededTime[i];
-                }else{
+                }
+                else    //if time in prev is greater , we add the current time in our ans
+                {   
                     ans += neededTime[i];
                 }
-            }else{
+            }
+            else    //if the prev character is not same , we just update our lastTime and character c
+            {
                 c = colors.charAt(i);
                 lastTime = neededTime[i];
             }
