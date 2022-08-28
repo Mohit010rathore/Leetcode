92ad659943e5e29c7f3de243571d084e8efd1820
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-08-28 13:24:53 +0530

    Time: 12 ms (89.23%), Space: 54.3 MB (71.73%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.java
@@ -1,16 +1,23 @@
 class Solution {
     public int minDeletions(String s) {
-        int deletion = 0;
+        
+        int deletion = 0; //to store the number pf deletions
         
         int [] freq = new int[26];
+        
+        //storing the frequencies of all character in  an array
         for(char c : s.toCharArray()){
             freq[c-'a']++;
             
         }
         
+        //Set will onlt contain the unique frequencies which is integer
         Set<Integer> uniqueFrequencies = new HashSet<Integer>();
         
         for(int count : freq){
+            
+            //if set contain the same frequency integer we do deletion++ and count of frequency--
+            //and if the frequency is unique we come out of while loop and add that uniqueFrequenct count in set
             while(count > 0 && uniqueFrequencies.contains(count)){
                 deletion++;
                 count--;
