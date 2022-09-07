c097e38eba7205ff322cbd400552d86ef787e3c1
Mohit Rathore <73990845+Mohit010rathore@users.noreply.github.com>
2022-09-07 23:45:01 +0530

    Time: 16 ms (5.65%), Space: 42.7 MB (23.59%) - LeetHub

---
+++ 228-summary-ranges/228-summary-ranges.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> ans = new ArrayList<>();
+        if(nums.length == 0){
+            return ans;
+        }
+        if(nums.length == 1){
+            ans.add(""+nums[0]);
+            return ans;
+        }
+        
+        int start = 0;
+        int i = 0;
+        
+        for(;start < nums.length && i < nums.length;){
+            if( i < nums.length-1 && nums[i] + 1 == nums[i+1]){
+                i++;
+            }else{
+                //otherwise we found out the range we have to add in the list
+                String subAns = "";
+                if(start == i){
+                    subAns += nums[start];
+                }else{
+                    subAns = nums[start] + "->" + nums[i];
+                }
+                
+                ans.add(subAns);
+                start = i + 1;
+                i++;
+            }
+        }
+        return ans;
+    }
+}
